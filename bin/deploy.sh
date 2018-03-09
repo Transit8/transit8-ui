14bd69f32492811c34d4c0ab9dd773698acc3e2d
Mike Cohen <mjoecohen@gmail.com>
2018-03-09 14:23:06 +0000

    deployment script debugging

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,7 +9,7 @@ DEPLOY_SERVER=$SERVER:/home/bob/deployment/brightblockjs
 
 printf "\nSyncing brightblockjs to $DEPLOY_SERVER\n\n"
 
-rsync -aP --quiet -e "ssh -p 7019" ../dist/ bob@$DEPLOY_SERVER
+rsync -aP --quiet -e "ssh -p 7019" dist/ bob@$DEPLOY_SERVER
 
 ssh -i ~/.ssh/id_rsa -p 7019 bob@$SERVER "
 	rsync -aP --quiet  /home/bob/deployment/brightblockjs/  rsync://localhost:10873/volume/deployments/nginx/html/brightblockjs
