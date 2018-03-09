57f094a9415d36bd2222db13e67b69e41251e13d
Mike Cohen <mjoecohen@gmail.com>
2018-03-09 13:49:06 +0000

    added deployment script to linode

---
+++ bin/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+#
+# Deploy the bidlogix application.
+#
+############################################################
+
+SERVER=$1
+DEPLOY_SERVER=$SERVER:/home/bob/deployment/brightblockjs
+
+printf "\nSyncing brightblockjs to $DEPLOY_SERVER"
+
+rsync -aP --quiet -e "ssh -p 7019" ../dist/ bob@$DEPLOY_SERVER
+
+ssh -i ~/.ssh/id_rsa -p 7901 bob@$SERVER "
+	rsync -aP --quiet  /home/bob/deployment/brightblockjs/  rsync://localhost:10873/volume/deployments/nginx/html/brightblockjs
+";
+
+printf "\nFinished brightblockjs nginx build and deployment."
