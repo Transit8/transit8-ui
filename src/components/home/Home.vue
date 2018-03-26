6a7139b2ab042b58d5c4f219a52718022b82a260
Mike Cohen <mjoecohen@gmail.com>
2018-03-26 14:37:13 +0100

    tweaked login button

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -6,7 +6,7 @@
         <h1 class="title is-1">Brighton Blockchain</h1>
         <h2 class="subtitle is-3">Building Decentralised Applications</h2>
         <h3 class="subtitle is-1">
-          <button class="button"><a href="dcms.brightblock.org">Content Block</a></button>
+          <button class="button"><a href="http://dcms.brightblock.org">Content Block</a></button>
         </h3>
       </div>
     </div>
