5ee35795b6df46b426a9fbbffdc3f65c3f22ebe6
Tihana Tadic Mitrovic <tihana.tadic-mitrovic@i-ways.hr>
2018-09-12 15:22:59 +0200

    navigation improvements

--- src/components/nav/Languages.vue
+++ src/components/nav/Languages.vue
@@ -1,5 +1,5 @@
 <template>
-  <ul class="list-inline language-menu col-md-5">
+  <ul class="list-inline language-menu col-md-3">
     <li v-for="(lang, index) of languages" :key="index">
       <a href="#" @click.prevent="changeLanguage(lang.iso)">{{lang.name}}</a>
     </li>
