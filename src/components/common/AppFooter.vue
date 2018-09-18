c20afc2d448c89ea50b713f39384c127ac0cef61
Tihana Tadic Mitrovic <tihana.tadic-mitrovic@i-ways.hr>
2018-09-18 15:35:05 +0200

    Homepage - footer ui
    put footer in a container, width adjustments

--- src/components/common/AppFooter.vue
+++ src/components/common/AppFooter.vue
@@ -1,33 +1,38 @@
 <template>
   <!-- Footer -->
   <footer class="black-bg">
-    <div class="footer-top clearfix">
-      <div class="col-sm-4 text-left text-uppercase">
+    <div class="container wide">
+    <div class="row footer-top clearfix">
+      <div class="col-md-5 col-sm-4 text-left text-uppercase">
         <ul class="list-inline">
           <li><router-link to="/about">About</router-link></li>
           <li><router-link to="/resources">Resources</router-link></li>
           <li><router-link to="/how-it-works">How it works</router-link></li>
         </ul>
       </div>
-      <div class="col-sm-8 text-left">
+      <div class="col-md-7 col-sm-8 text-left">
         <newsletter-form @submit="submitNewsletterEmail($event)"/>
       </div>
     </div>
-    <div class="footer-bottom clearfix">
-      <div class="col-sm-6">
-        <ul class="list-inline">
-          <li class="text-uppercase "><span class="copy">&copy;</span>2018 Sybella</li>
-          <li><router-link to="/privacy">Privacy Policy</router-link></li>
-          <li>Karl Marx Allee 98, 10243 Berlin, DE</li>
-        </ul>
-      </div>
-      <div class="col-sm-6 social">
-        <ul class="list-inline ">
-          <li class="text-uppercase">Join the conversation</li>
-          <li><a href="#"><span class="icon-email"></span></a></li>
-          <li><a href="#"><span class="icon-m_social"></span></a></li>
-          <li><a href="#"><span class="icon-instagram"></span></a></li>
-        </ul>
+    </div>
+    <div class="divider divider-grey"></div>
+     <div class="container wide">
+      <div class="row footer-bottom clearfix">
+        <div class="col-md-7 col-sm-6">
+          <ul class="list-inline">
+            <li class="text-uppercase "><span class="copy">&copy;</span>2018 Sybella</li>
+            <li><router-link to="/privacy">Privacy Policy</router-link></li>
+            <li>Karl Marx Allee 98, 10243 Berlin, DE</li>
+          </ul>
+        </div>
+        <div class="col-md-5 col-sm-6 social">
+          <ul class="list-inline ">
+            <li class="text-uppercase">Join the conversation</li>
+            <li><a href="#"><span class="icon-email"></span></a></li>
+            <li><a href="#"><span class="icon-m_social"></span></a></li>
+            <li><a href="#"><span class="icon-instagram"></span></a></li>
+          </ul>
+        </div>
       </div>
     </div>
   </footer>
